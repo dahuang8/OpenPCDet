607b8a4a4c3dd368315b6e16c22edd0693a15960
Kevin Huang <kevin.huang@midea.com>
2021-10-06 15:01:22 -0700

    more tools

--- tools/file_utils/velodyne.py
+++ tools/file_utils/velodyne.py
@@ -16,3 +16,16 @@ def convert_bin_to_pcd(bin_file):
     pcd = o3d.geometry.PointCloud()
     v3d = o3d.utility.Vector3dVector
     pcd.points = v3d(np_pcd)
+
+
+def load_kitti_pc(file_path):
+    list_pcd = list()
+    SIZE_FLOAT = 4
+    with open(file_path, "rb") as f:
+        byte = f.read(SIZE_FLOAT * 4)
+        while byte:
+            x, y, z, intensity = struct.unpack("ffff", byte)
+            list_pcd.append([x, y, z])
+            byte = f.read(SIZE_FLOAT * 4)
+    np_pcd = np.asarray(list_pcd)
+    return np_pcd
